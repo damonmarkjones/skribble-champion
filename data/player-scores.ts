cc129e2e54f492a707de8ca3c1a490d6a520bfe4
Damon Jones <damon.mark.jones@outlook.co>
2023-01-13 16:13:51 +0000

    update scores for 13th Jan

--- data/player-scores.ts
+++ data/player-scores.ts
@@ -7,47 +7,47 @@ export interface Player {
 const players: Player[] = [
   {
     name: "Jen",
-    played: 1,
+    played: 2,
     wins: 1,
   },
   {
     name: "Damo",
-    played: 1,
-    wins: 0,
+    played: 2,
+    wins: 1,
   },
   {
     name: "Patt",
-    played: 1,
+    played: 2,
     wins: 0,
   },
   {
     name: "Mally",
-    played: 1,
+    played: 2,
     wins: 0,
   },
   {
     name: "Tom",
-    played: 0,
+    played: 1,
     wins: 0,
   },
   {
     name: "Qal",
-    played: 1,
+    played: 2,
     wins: 0,
   },
   {
     name: "Edward",
-    played: 1,
+    played: 2,
     wins: 0,
   },
   {
     name: "Eva",
-    played: 1,
+    played: 2,
     wins: 0,
   },
   {
     name: "Davide",
-    played: 1,
+    played: 2,
     wins: 0,
   },
 ];
