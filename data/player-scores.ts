ae2af32791931168470a89ac493174970e66012b
Damon Jones <damon.mark.jones@outlook.co>
2023-01-27 16:28:08 +0000

    update scores for 27th Jan

--- data/player-scores.ts
+++ data/player-scores.ts
@@ -13,30 +13,30 @@ export interface HighScore {
 }
 
 export const highScore: HighScore = {
-  score: 2250,
-  player: "Davide",
-  date: DateTime.fromFormat("2023-01-20", "yyyy-mm-dd"),
+  score: 5615,
+  player: "Liam",
+  date: DateTime.fromFormat("2023-01-27", "yyyy-mm-dd"),
 };
 
 const players: Player[] = [
   {
     name: "Jen",
-    played: 2,
+    played: 3,
     wins: 1,
   },
   {
     name: "Damo",
-    played: 3,
+    played: 4,
     wins: 1,
   },
   {
     name: "Patt",
-    played: 2,
+    played: 3,
     wins: 0,
   },
   {
     name: "Mally",
-    played: 3,
+    played: 4,
     wins: 0,
   },
   {
@@ -46,29 +46,34 @@ const players: Player[] = [
   },
   {
     name: "Qal",
-    played: 3,
+    played: 4,
     wins: 0,
   },
   {
     name: "Edward",
-    played: 2,
+    played: 3,
     wins: 0,
   },
   {
     name: "Eva",
-    played: 3,
+    played: 4,
     wins: 0,
   },
   {
     name: "Davide",
-    played: 3,
+    played: 4,
+    wins: 1,
+  },
+  {
+    name: "Liam",
+    played: 1,
     wins: 1,
   },
 ];
 
 export const playerScores = () => {
   const scores = players.sort(
-    (a: Player, b: Player) => b.wins - a.wins || b.played - a.played
+    (a: Player, b: Player) => b.wins - a.wins || a.played - b.played
   );
   return scores;
 };
